e22e50e386abf2da7c8985538d54b49c148f2b5a
Tymon Woźniak <tymonek12345@gmail.com>
2024-02-29 10:25:32 +0100

    Stone unique placement and fix PI approximation

--- main.js
+++ main.js
@@ -1,61 +1,19 @@
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-let btnRzuc = document.getElementById("btn-rzuc");
-let btnPoliczPi = document.getElementById("btn-policzPi");
-let iloscRzuconychKamieni = 1000;
-let x = 0;
-let y = 0;
-let iloscKamieniWKole = 0;
-let tabX = []; // tablica współrzędnych x rzuconych kamieni
-let tabY = [];
-let r = 300;  // promień koła
-let PI = 0;
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
-rysuj();   // rysuje
+const btnThrow = document.getElementById("btn-stone-throw");
+const btnClear = document.getElementById("btn-stone-clear");
+const btnComputePi = document.getElementById("btn-compute-pi");
 
-function rysuj() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
-    rysujUkladWspolrzednych();
-    rysujKwadrat();
-    rysujKolo();
-}
+const amountThrowingStones = 400 * 400;
+// circle radius
+const r = 300;
 
-// rzucanie kamieniami
-btnRzuc.onclick = function () {
-    alert("I throw :)");
-    // czyścimy tablice z współrzędnymi
-    tabX = [];
-    tabY = [];
-    //alert(tabX.length + " pocz " + tabY.length);
-    for (let i = 0; i < iloscRzuconychKamieni; i++) {
-        // Returns a random integer from 100 to 700:
-        x = Math.floor(Math.random() * 601) + 100;
-        y = Math.floor(Math.random() * 601) + 100;
-        ctx.fillRect(x, y, 2, 2); // fill rect in at (x,y)
-        // dodaj współrzędne do tablicy
-        tabX.push(x);
-        tabY.push(y);
-    }
-    //alert(tabX.length + " " + tabY.length);
-}
+const stones = new Set();
 
-// oblicza PI
-btnPoliczPi.onclick = function () {
-    iloscKamieniWKole = 0;
-    // zliczamy ilosć kamieni w kole
-    // https://www.matemaks.pl/rownanie-okregu.html
-    // (x−a)2+(y−b)2=r2
-    for (let i = 0; i < iloscRzuconychKamieni; i++) {
-        if ((tabX[i] - 400) * (tabX[i] - 400) + (tabY[i] - 400) * (tabY[i] - 400) <= r * r) {
-            iloscKamieniWKole++;
-        }
-    }
-    alert("number of stones in the circle: " + iloscKamieniWKole);
-    PI = 4 * iloscKamieniWKole / iloscRzuconychKamieni;
-    alert("PI: " + PI);
-}
+let computed_pi = 0;
 
-function rysujUkladWspolrzednych() {
+function drawCoordinateSystem() {
     ctx.beginPath(); // Start a new path
     ctx.moveTo(400, 800); // Move the pen
     ctx.lineTo(400, 0); // Draw a line
@@ -64,7 +22,7 @@ function rysujUkladWspolrzednych() {
     ctx.stroke(); // Render the path
 }
 
-function rysujKwadrat() {
+function drawSquare() {
     ctx.strokeStyle = "blue";
     ctx.beginPath(); // Start a new path
     ctx.moveTo(100, 100);
@@ -75,9 +33,67 @@ function rysujKwadrat() {
     ctx.stroke(); // Render the path
 }
 
-function rysujKolo() {
+function drawCircle() {
     ctx.strokeStyle = "red";
     ctx.beginPath(); // Start a new path
     ctx.arc(400, 400, 300, 0, 2 * Math.PI);
     ctx.stroke(); // Render the path
 }
+
+const drawStones = () => {
+    stones.forEach((stone) => {
+        ctx.fillRect(stone.x, stone.y, 1, 1);
+    });
+}
+
+function redraw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCoordinateSystem();
+    drawSquare();
+    drawCircle();
+    drawStones();
+}
+
+const getRandomInRange = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const isInCircle = (x, y) => Math.pow(x - 400, 2) + Math.pow(y - 400, 2) <= Math.pow(r, 2);
+
+const getStones = () => stones.size;
+
+const getStonesInCircle = () => {
+    let stonesInCircle = 0;
+    stones.forEach((stone) => {
+        if (isInCircle(stone.x, stone.y)) stonesInCircle += 1;
+    });
+    return stonesInCircle;
+}
+
+function throwStones() {
+    for (let i = 0; i < amountThrowingStones; i++) {
+        // Randomises x and y coordinate <100; 700>
+        const x = getRandomInRange(100, 700);
+        const y = getRandomInRange(100, 700);
+        stones.add({ x: x, y: y });
+    }
+    redraw();
+}
+
+function computePi() {
+    let stones = getStones();
+    let stonesInCircle = getStonesInCircle();
+    computed_pi = 4 * stonesInCircle / stones;
+    alert(`Total stones: ${stones}\nStones in the circle: ${stonesInCircle}\nPI approximation: ${computed_pi}\nPrecission: ${computed_pi / Math.PI * 100}%`);
+}
+
+btnThrow.addEventListener('click', throwStones);
+btnComputePi.addEventListener('click', computePi);
+btnClear.addEventListener('click', () => {
+    stones.clear();
+    redraw();
+})
+
+redraw();
